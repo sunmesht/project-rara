ae8470057b1d6d125cdd9b67aaf7399062757c16
sunmesht <matchamilky.stdy@gmail.com>
2026-06-13 20:22:58 +0700

    first commit: react app untuk tugas PWL

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Halo, ini deployment pertamaku! 🎉</h1>
+        <p>Nama: Rara</p>
+        <p>NIM: ISI_NIM_KAMU</p>
+        <p>Tugas Praktikum PWL - Deploy ke Vercel</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
